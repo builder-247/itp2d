19f549d56ae7b69b1393fc19836682d10b905c49
Perttu Luukko <perttu.luukko@iki.fi>
2012-02-06 12:50:00 +0200

    plot_potential: add option for using mayavi
    Mayavi plots 3D images a lot faster than matplotlib.

--- scripts/plot_potential.py
+++ scripts/plot_potential.py
@@ -4,15 +4,16 @@ from math import *
 import sys, os
 import h5py
 from optparse import OptionParser
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm, pyplot, ticker
 from numpy import *
 
 # Plot a stored potential from an itp2d datafile
 
 if __name__=="__main__":
     parser = OptionParser(usage="%prog datafile.h5")
-    parser.add_option("-2", "--2d", action="store_true", dest="twodee")
+    parser.add_option("-2", "--2d", action="store_true", dest="twodee",
+            help="Plot 2D projection of potential")
+    parser.add_option("", "--matplotlib3d", action="store_true", dest="matplotlib3d",
+            help="Plot 3D image using matplotlib. Very slow.")
     (options, args) = parser.parse_args()
 
     try:
@@ -36,19 +37,34 @@ if __name__=="__main__":
     extent = array([min(X), max(X), min(Y), max(Y)])
     X, Y = meshgrid(X, Y)
     # Actual plotting
-    fig = pyplot.figure(figsize=(12,12))
-    if options.twodee:
-        projection = "rectilinear"
+    if options.twodee or options.matplotlib3d:
+        # Plot using matplotlib
+        from matplotlib import cm, pyplot, ticker
+        from mpl_toolkits.mplot3d import Axes3D
+        fig = pyplot.figure(figsize=(12,12))
+        ax = fig.add_subplot(1, 1, 1, projection=("rectilinear" if options.twodee else "3d"), aspect="equal")
+        if options.twodee:
+            ax.xaxis.set_major_locator(ticker.NullLocator())
+            ax.yaxis.set_major_locator(ticker.NullLocator())
+            ax.imshow(potential, extent=extent, rasterized=True)
+        else:
+            ax.set_xlabel('$x$')
+            ax.set_ylabel('$y$')
+            ax.plot_surface(X, Y, potential, rstride=1, cstride=1, cmap=cm.jet, linewidth=0)
+            ax.apply_aspect()
+        pyplot.show()
     else:
-        projection = "3d"
-    ax = fig.add_subplot(1, 1, 1, projection=projection, aspect="equal")
-    if options.twodee:
-        ax.xaxis.set_major_locator(ticker.NullLocator())
-        ax.yaxis.set_major_locator(ticker.NullLocator())
-        ax.imshow(potential, extent=extent, rasterized=True)
-    else:
-        ax.set_xlabel('$x$')
-        ax.set_ylabel('$y$')
-        ax.plot_surface(X, Y, potential, rstride=1, cstride=1, cmap=cm.jet, linewidth=0)
-        ax.apply_aspect()
-pyplot.show()
+        # Plot using mayavi
+        import mayavi
+        from mayavi import mlab
+        fig = mlab.figure(bgcolor=(1,1,1))
+        light_manager = fig.scene.light_manager
+        light_manager.number_of_lights = 4
+        lights = light_manager.lights
+        for light in lights:
+            light.activate = True
+            light.intensity = 0.5
+        X = transpose(X)
+        Y = transpose(Y)
+        s = mlab.surf(X, Y, potential, warp_scale='auto')
+        mlab.show()
