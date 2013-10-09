77ccbe14acfc057128180f9bb583537403071694
Perttu Luukko <perttu.luukko@iki.fi>
2013-10-09 08:46:25 +0300

    Add trim and scale to avg density to itp_draw_art

--- scripts/itp_draw_art.py
+++ scripts/itp_draw_art.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python2
-from __future__ import division, with_statement
-import sys, os
+from __future__ import division
+import os
 import h5py
-from numpy import *
+from math import floor, ceil, sqrt
+from numpy import flipud
 from optparse import OptionParser
-from PIL import Image, ImageFilter
+from PIL import Image
 
 # Print pretty pictures from the wave functions computed with itp2d.
 
@@ -27,17 +28,21 @@ except ImportError:
 if __name__ == "__main__":
     # parse command line arguments
     parser = OptionParser(usage="%prog [options] [datafile.h5] [indices]")
-    parser.set_defaults(verbose=True, combined=True, colorscheme="default", slot=-1, margin=0)
+    parser.set_defaults(verbose=True, combined=True, colorscheme="default",
+            slot=-1, margin=0, trim=0, average_point=0)
     parser.add_option("-v", "--verbose", action="store_true")
     parser.add_option("-q", "--quiet", action="store_false", dest="verbose")
     parser.add_option("-o", "--output", type="string", metavar="FILENAME", help="Filename for the output image")
     parser.add_option("-a", "--all", action="store_true", help="Draw also the extra states not intended to converge")
     parser.add_option("-s", "--square", action="store_true", help="Discard states so that the resulting image is square")
     parser.add_option("-m", "--margin", type="int", metavar="PIXELS", help="Add some empty space between states in the combined image")
+    parser.add_option("-t", "--trim", type="int", metavar="PIXELS", help="Remove some space from the edges of each state image")
     parser.add_option(      "--separate", action="store_true", help="Save separate images of each state")
     parser.add_option(      "--no-combined", action="store_false", dest="combined", help="Do not save a combined image of all the states")
     parser.add_option("-c", "--colorscheme", type="choice", choices=colorschemes.keys(), help="The colors. Valid choices: %s" % colorschemes.keys())
     parser.add_option("-S", "--slot", type="int", help="If the datafile contains several sets of states this lets you select the one you want. The default is to load the last one.")
+    parser.add_option(      "--scale-to-average-point", type="float", metavar="VAL", dest="average_point", default=0,
+            help="Instead of scaling density data so that 1.0 corresponds to maximum density, scale so that VAL corresponds to average density.")
     (options, args) = parser.parse_args()
     if (len(args) > 0):
         filename = args[0]
@@ -66,8 +71,9 @@ if __name__ == "__main__":
     if ((options.slot >= 0) and options.slot >= states.shape[0]) \
             or ((options.slot < 0) and abs(options.slot) > states.shape[0]):
         parser.error("Invalid slot index %d. Datafile has %d slots." % (options.slot, states.shape[0]))
-    Mx = file.attrs["grid_sizex"]
-    My = file.attrs["grid_sizey"]
+    trim = options.trim
+    Mx = file.attrs["grid_sizex"] - 2*trim
+    My = file.attrs["grid_sizey"] - 2*trim
     # The states that are drawn are either
     # * The ones specified by the indices argument
     # * All states found in the file (if --all)
@@ -96,9 +102,13 @@ if __name__ == "__main__":
     for index in indices:
         # The data to plot is the square of the absolute value of the
         # wave function, i.e., the probability density
-        Z = abs(states[options.slot, index])**2
+        Z = abs(states[options.slot, index][trim:-trim,trim:-trim])**2
         # Normalize
-        Z /= Z.max()
+        if options.average_point == 0:
+            Z /= Z.max()
+        else:
+            avg = Z.mean()
+            Z *= options.average_point/avg
         # Flatten the data array, map each value to a color, and write to the image
         # It seems that PIL writes data in reverse order (bottom up), so we need to
         # flip the data array with flipud
